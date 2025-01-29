432f7af7191aacbbe7ca8f6fc212bcfe6e9834c2
Ahmed KADIM <kadim.ahmed.web@gmail.com>
2025-01-29 16:40:17 +0100

    [BE] setting up the backend project

---
+++ backend/centralperk/src/main/java/com/invenboost/centralperk/CentralperkApplication.java
@@ -0,0 +1,13 @@
+package com.invenboost.centralperk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralperkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralperkApplication.class, args);
+	}
+
+}
---
+++ backend/centralperk/src/test/java/com/invenboost/centralperk/CentralperkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invenboost.centralperk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralperkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
