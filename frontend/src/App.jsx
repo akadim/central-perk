60d313d285992fe95a5d4fd964c9f82209426cb8
Ahmed KADIM <kadim.ahmed.web@gmail.com>
2025-01-30 01:50:05 +0100

    [FE] Adding the Sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,27 @@
+import { CssBaseline, ThemeProvider, Box, createTheme } from '@mui/material'
+
 import './App.css'
+import { useState } from 'react'
+import Sidebar from './components/Sidebar'
 
 function App () {
-  return <></>
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#2E8B57'
+      }
+    }
+  })
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <Sidebar />
+        <Box component='main' sx={{ flexGrow: 1, p: 3 }}></Box>
+      </Box>
+    </ThemeProvider>
+  )
 }
 
 export default App
