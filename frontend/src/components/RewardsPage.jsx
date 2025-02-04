59e84ef5c1422928e764d66f236640261ccb2443
Ahmed KADIM <kadim.ahmed.web@gmail.com>
2025-02-04 21:43:29 +0100

    Fixing the Re-rendering of rewards List

--- frontend/src/components/RewardsPage.jsx
+++ frontend/src/components/RewardsPage.jsx
@@ -1,12 +1,35 @@
 import { Container } from '@mui/material'
 import CategoriesList from './CategoriesList'
 import RewardsList from './RewardsList'
+import { RewardModal } from './modals/RewardModal'
+import { useCallback, useState } from 'react'
 
 const RewardsPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [modalMode, setModalMode] = useState('add')
+  const [selectedReward, setSelectedReward] = useState(null)
+
+  const showRewardForm = useCallback(reward => {
+    setModalMode(reward ? 'update' : 'add')
+    setSelectedReward(reward)
+    setIsModalOpen(true)
+  }, [])
+
+  const handleCloseModal = useCallback(() => {
+    setIsModalOpen(false)
+    setSelectedReward(null)
+  }, [])
+
   return (
     <Container maxWidth='lg' sx={{ py: 4 }}>
       <CategoriesList />
-      <RewardsList />
+      <RewardsList onShowModalForm={showRewardForm} />
+      <RewardModal
+        open={isModalOpen}
+        onClose={handleCloseModal}
+        mode={modalMode}
+        initialData={selectedReward}
+      />
     </Container>
   )
 }
